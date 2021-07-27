ca737bbc68666cec41c14af71d9002d44e454b91
hotShot1211 <anshusuperstar15@gmail.com>
2021-07-27 21:49:29 +0530

    js/ui & xtr-crdts

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,47 @@
 let firstNumber = '';
-let operator;
+let currOperator = '';
+let oldOperator = ''
 let secondNumber = '';
+let ans = '';
 
-function add(a, b){
+function add(a, b) {
     return Number(a) + Number(b);
 }
 
-function subtract(a, b){
+function subtract(a, b) {
     return Number(a) - Number(b);
 }
 
-function multiply(a, b){
+function multiply(a, b) {
     return Number(a) * Number(b);
 }
 
-function divide(a, b){
-    return Number(a) / Number(b);
-    // if(b != 0){
-    // }
-    // else{
-    //     return NaN;
-    // }
+function divide(a, b) {
+    if (b != 0) {
+        return Number(a) / Number(b);
+    }
+    else {
+        return 'You are better thant his!';
+    }
 }
 
-function operate(a, operator,c){
-    if(operator == '+'){
+function operate(a, operator, c) {
+
+    if (operator == '+') {
         return add(a, c);
     }
-    if(operator == '-'){
+    if (operator == '-') {
         return subtract(a, c);
     }
-    if(operator == '*'){
+    if (operator == '*') {
         return multiply(a, c);
     }
-    if(operator == '/'){
+    if (operator == '/') {
         return divide(a, c);
+    }
+    if (operator == '=') {
+        firstNumber= '';
+        return a;
     }
 }
 
@@ -45,13 +52,70 @@ let display = document.querySelector('.display');
 
 let buttons = document.querySelectorAll('.btn');
 
-buttons.forEach(button =>{
-    button.addEventListener('click', (e) =>{
-        if(firstNumber.length < 10){
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (currOperator == '') {
             firstNumber += button.innerText;
             display.innerText = firstNumber;
-            console.log(firstNumber.length);
         }
-        
+        else {
+            display.innerText = '';
+            secondNumber += button.innerText;
+            display.innerText = secondNumber;
+
+        }
     });
 });
+
+
+let operatorKeys = document.querySelectorAll('.operator');
+
+operatorKeys.forEach(operatorKey => {
+    operatorKey.addEventListener('click', (e) => {
+        // let x = (e.currentTarget).getAttribute('class');
+        // console.log(x);
+        oldOperator = currOperator;
+        currOperator = operatorKey.innerText;
+        if (firstNumber != '' && secondNumber != '' && oldOperator != '') {
+            ans = operate(firstNumber, oldOperator, secondNumber);
+            display.innerText = ans;
+            if(currOperator != '='){
+                firstNumber = ans;
+                console.log(ans);
+                ans = '';
+                secondNumber = '';
+            }
+            else{
+                console.log('viva la vida')
+                let currnTrgt = (e.currentTarget).getAttribute('class');
+                console.log(currnTrgt);
+                if(currnTrgt == 'operator'){
+                    firstNumber = ans;
+                    console.log(ans);
+                    ans = '';
+                    secondNumber = '';
+                    console.log('hi');
+                }
+                else{
+                    firstNumber = '';
+                    currOperator = '';
+                    oldOperator = ''
+                    secondNumber = '';
+                    ans = '';
+                }
+            }
+        }
+
+    })
+})
+
+let clear = document.querySelector('.functn-btn-AC');
+
+clear.addEventListener('click', () => {
+    firstNumber = '';
+    currOperator = '';
+    oldOperator = ''
+    secondNumber = '';
+    ans = '';
+    display.innerText = '0';
+})
---
+++ test.js
@@ -0,0 +1,3 @@
+let a = 'abc'
+let b = a;
+console.log(b);
